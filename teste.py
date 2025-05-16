118f986f337353154ff345774703c513c008b2d2
mtuliom <mtulio@academico.ufs.br>
2025-05-16 10:15:25 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,7 +2,7 @@ import time
 import random
 import string
 
-# Lista global que acumula dados desnecessariamente 7
+# Lista global que acumula dados desnecessariamente 8
 memory_leak_simulation = []
 
 def generate_random_string(size=10000):
