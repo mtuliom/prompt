7d64c60ebf9075bf7d4965134a4a026fb6f4f88d
mtuliom <mtulio@academico.ufs.br>
2025-05-23 09:57:03 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -1,5 +1,5 @@
 leak = []
 
 while True:
-    leak.append("x" * 10**6)  # Adiciona 1 MB de dados a cada loop 4
+    leak.append("x" * 10**6)  # Adiciona 1 MB de dados a cada loop 5
     print(f"Tamanho da lista: {len(leak)}")
